11623ec59854c3fa43896c8d86e0aafd2a75cf83
edmundontf <edmundontf@gmail.com>
2025-02-27 08:33:21 -0300

    feat: arquivo de entrada para executar projeto

---
+++ src/main/kotlin/com/projeto_mentoria/por_service/PortServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.projeto_mentoria.por_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortServiceApplication>(*args)
+}
